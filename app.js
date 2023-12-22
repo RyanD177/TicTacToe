9e776b8b7f1d406ce7e72e32746a3e9b94fb1350
RyanD177 <w0477770@campus.nscc.ca>
2023-12-22 10:05:33 -0400

    ez

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ let restartButton = document.getElementById('restart-button')
 gameSquares.forEach((gameSquare, i) => {
  gameSquare.addEventListener('click', ()  => {
   const row = Math.floor(i / boardWidth);
-  const col = Math.floor(i % boardWidth);
-  makeMove(gameHeading, row, col);
+  const col = i %  boardWidth;
+  makeMove(gameSquare, row, col);
 
  })
 
@@ -25,14 +25,21 @@ restartButton.addEventListener('click', restartGame);
 
 
 function makeMove(gameSquare, row, col) {
- 
- 
+gameSquare.textContent = currentPlayer ===  1 ? "X" : "O"
   gameSquare.disabled = true; 
+  boardState[row][col] = currentPlayer;
   numMovesDone++
 
+  if(didPlayerWin(currentPlayer)){
+    gameHeading.textContent = `Player ${currentPlayer} won`
+   
 
-
-
+  }else if(numMovesDone >= boardWidth * boardWidth){
+    gameHeading.textContent = `It is a tie`
+  }else{
+    currentPlayer = currentPlayer === 1 ? 2 : 1;
+    changePlayerHeading();
+  }
 
 }
 
@@ -42,23 +49,60 @@ gameHeading.textContent = `It is ${currentPlayer} turn`
 }
 
 function didPlayerWin(currentPlayer) {
+const rows = [0,1,2];
+const wonHorizontal = rows.some((row) => {
+
+  return(
+    boardState[row][0] === currentPlayer &&
+    boardState[row][1] === currentPlayer &&
+    boardState[row][2] === currentPlayer 
+
+  )
 
 
+})
+
+const cols = [0,1,2];
+
+const wonVeritcal = cols.some((col) => {
+  return (
+
+    boardState[0][col] === currentPlayer &&
+    boardState[1][col] === currentPlayer &&
+    boardState[2][col] === currentPlayer 
+  )
+
+})
+
+
+return (
+  
+  wonHorizontal || wonVeritcal
+
+)
 }
  
   
   
 
 
-function endGame() {
 
-
-}
 function restartGame(){
+  boardState = generateEmptyBoardState()
+  currentPlayer =1;
+  numMovesDone = 0;
+  changePlayerHeading();
+  gameSquares.forEach((gameSquare) => {
+  gameSquare.textContent = "";
+  gameSquare.disabled = false;
+
+  })
+  restartButton.style.display = "none";
 
 }
 
 function generateEmptyBoardState() {
-
-}
+  return new Array(boardWidth).fill()
+  .map(() => new Array(boardWidth).fill());
+  }
 
